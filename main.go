73751752c6fa48010bf5269b65f6950a802b5ff8
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-06-26 22:29:24 -0300

    separating http config in its own module

--- main.go
+++ main.go
@@ -1,39 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/vitorvidaldev/MSN/config"
 )
 
 func main() {
-	credential := options.Credential{
-		Username: "msn_",
-		Password: "mongo_password",
-	}
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(credential))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(databases)
+	config.MongoConfig()
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Hello, you've requested: %s\n", r.URL.Path)
