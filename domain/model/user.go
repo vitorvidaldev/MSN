4ead8b3d193f2afd5e6081c20d9fc5bd9f3088d3
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-07-02 19:21:06 -0300

    adding hash and verify methods

--- domain/model/user.go
+++ domain/model/user.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -9,8 +11,17 @@ type User struct {
 	ID        string    `json:"_id,omitempty" bson:"_id,omitempty"`
 	Username  string    `json:"username" bson:"username,omitempty"`
 	Email     string    `json:"email" bson:"email,omitempty"`
-	Password  string    `json:"password" bson:"password,omitempty"`
-	Salt      string    `json:"-" bson:"salt"`
+	Hash      string    `json:"hash" bson:"hash,omitempty"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
