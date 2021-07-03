2564c290d39a4ffc6dc80328e09eba06cde1bedf
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-07-03 14:24:30 -0300

    initial commit

---
+++ test/user_controller_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestUsersCRUD(t *testing.T) {
+	// t.Run()
+}
