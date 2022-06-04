54dde0e02f66a1dac9c15765dca6d35fb6a280e4
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-06-04 18:52:21 -0300

    Updating user repository

--- src/domain/repository/user_repository.go
+++ src/domain/repository/user_repository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"log"
-
 	"github.com/google/uuid"
 	"github.com/vitorvidaldev/msn/src/config"
 	"github.com/vitorvidaldev/msn/src/domain/entity"
@@ -11,12 +9,9 @@ import (
 func InsertUser(user entity.User) entity.User {
 	db := config.InitPGConnection()
 
-	sqlStatement := `insert into user (user_id, email, username, password) values ($1, $2, $3, $4)`
+	sqlStatement := `insert into "user"(user_id, email, username, password) values ($1, $2, $3, $4)`
 
-	err := db.QueryRow(sqlStatement, user.User_id, user.Email, user.Username, user.Password)
-	if err != nil {
-		log.Fatal("Error inserting use r", err)
-	}
+	db.QueryRow(sqlStatement, user.User_id, user.Email, user.Username, user.Password)
 
 	defer db.Close()
 	return user
